87fd357fa84f9d1261796703b5f09fe0384c0738
gdssvpp <pablonogueira16@hotmail.com>
2022-01-30 13:30:30 -0300

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso intensivo voltado a Linguagem Java");
+        curso1.setCargaHoraria(54);
 
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso IDEs");
+        curso2.setDescricao("Curso intensivo sobre IDEs");
+        curso2.setCargaHoraria(15);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria da Linguagem Java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
+
 }
